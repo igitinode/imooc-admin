f88389f3eb704d3174c57657612fd84584ebf8f6
igitinode <igitinode@gmail.com>
2024-09-07 22:27:06 +0800

    chore(custom): 修改构建工具配置文件，增加自动保存执行 prettier 规则

--- .cz-config.js
+++ .cz-config.js
@@ -18,13 +18,16 @@ module.exports = {
   // 消息步骤
   messages: {
     type: '请选择提交类型:',
+    // customScope 要启用，需要配置 allowCustomScopes: true,
     customScope: '请输入修改范围(可选):',
     subject: '请简要描述提交(必填):',
     body: '请输入详细描述(可选):',
     footer: '请输入要关闭的issue(可选):',
     confirmCommit: '确认使用以上信息提交？(y/n/e/h)'
   },
-  // 跳过问题
+  customScope: true,
+  allowCustomScopes: true,
+  // 跳过问题, messages 里面配置的所有选项
   skipQuestions: ['body', 'footer'],
   // subject文字长度默认是72
   subjectLimit: 72
