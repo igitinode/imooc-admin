2b01799bf309c2428c6a3c6643909d1063086590
Jack <igitinode@gmail.com>
2024-09-09 08:52:29 +0800

    chore(utils): 格式修改

--- .cz-config.js
+++ .cz-config.js
@@ -16,12 +16,12 @@ module.exports = {
     { value: 'build', name: 'build:    打包' }
   ],
   // 可选 customerScope
-//   customScope: {
-//     global: 'Global',
-//     custom: '',
-//     ui: 'UI Components',
-//     api: 'API Service'
-//   },
+  //   customScope: {
+  //     global: 'Global',
+  //     custom: '',
+  //     ui: 'UI Components',
+  //     api: 'API Service'
+  //   },
   // scope 类型（定义之后，可通过上下键选择）
   scopes: [
     ['components', '组件相关'],
@@ -43,11 +43,11 @@ module.exports = {
   // 消息步骤
   messages: {
     type: '请选择提交类型:',
-    scope: '\n选择一个 scope(可选)：',
+    scope: '\n选择一个 scope(可选): ',
     // customScope: '请输入修改范围(可选):',
-    subject: '请简要描述提交(必填):',
-    body: '请输入详细描述(可选):',
-    footer: '请输入要关闭的issue(可选):',
+    subject: '请简要描述提交(必填): ',
+    body: '请输入详细描述(可选): ',
+    footer: '请输入要关闭的issue(可选): ',
     confirmCommit: '确认使用以上信息提交？(y/n/e/h)'
   },
   // 允许自定义范围
