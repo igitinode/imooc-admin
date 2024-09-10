ab271b5726f007b67857d0276f74b2e524e0f954
Jack <igitinode@gmail.com>
2024-09-10 14:57:08 +0800

    feat(components): 完成本地缓存处理方案（LocalStorage）

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,10 +1,21 @@
 import { login } from '@/api/sys'
 import md5 from 'md5'
+import { setItem, getItem } from '@/utils/storage'
+import { TOKEN } from '@/constant'
 
 export default {
   namespaced: true, // 此模块的单独模块，不会被合并到主模块里面去
-  state: () => ({}),
-  mutations: {},
+  state: () => ({
+    // 初始值, 刷新以及重进浏览器的时候自动获取token
+    token: getItem(TOKEN) || ''
+  }),
+  mutations: {
+    setToken(state, token) {
+      state.token = token
+      // 将 token 存一份在 localStorage 中
+      setItem(TOKEN, token)
+    }
+  },
   actions: {
     /**
      * 登录请求动作
@@ -14,10 +25,14 @@ export default {
       // 无论登录成功还是失败，都希望在组建里面进行处理，
       return new Promise((resolve, reject) => {
         login({
+          icode: 'hellosunday',
           username,
           password: md5(password)
         })
           .then(data => {
+            // 调用 mutations 的 setToken 方法
+            // 将 token 保存在 mutations 里面
+            this.commit('user/setToken', data.data.data.token)
             resolve()
           })
           .catch(err => {
