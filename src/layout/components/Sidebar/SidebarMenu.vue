bb74004402bf80e0adfe844644d6aa57a73566ba
Jack <igitinode@gmail.com>
2024-09-12 12:19:28 +0800

    feat(components): 4.15 业务落地：修复参与问题-样式问题-路由跳转-默认激活项

--- src/layout/components/Sidebar/SidebarMenu.vue
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -1,11 +1,14 @@
 <template>
   <!-- 一级 menu 菜单 -->
+  <!-- $store.getters.cssVar 是解决样式问题 -->
+  <!-- router 是解决路由跳转问题 -->
   <el-menu
     :uniqueOpened="true"
-    default-active="2"
-    background-color="#545c64"
-    text-color="#fff"
-    active-text-color="#ffd04b"
+    :default-active="activeMenu"
+    :background-color="$store.getters.cssVar.menuBg"
+    :text-color="$store.getters.cssVar.menuText"
+    :active-text-color="$store.getters.cssVar.menuActiveText"
+    router
   >
     <sidebar-item
       v-for="item in routes"
@@ -17,7 +20,7 @@
 
 <script setup>
 import { computed } from 'vue'
-import { useRouter } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { filterRouters, generateMenus } from '@/utils/route'
 import SidebarItem from './SidebarItem.vue'
 
@@ -25,6 +28,13 @@ const router = useRouter()
 const routes = computed(() => {
   const filterRoutes = filterRouters(router.getRoutes())
   return generateMenus(filterRoutes)
+})
+
+// 解决每个菜单默认激活项子菜单问题,并且样式高亮
+const route = useRoute()
+const activeMenu = computed(() => {
+  const { path } = route
+  return path
 })
 </script>
 
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,4 +1,7 @@
 // 功能：提供快捷访问方式，减少...的引用
+// 引入全局的样式变量，并提供给组件快捷访问，避免样式被绑死
+import variables from '@/styles/variables.module.scss'
+
 // 注意，getters 的属性其实是从 state 里面获取过来重新定义的
 const getters = {
   token: state => state.user.token,
@@ -11,7 +14,8 @@ const getters = {
     // 不能简单使用 return !state.user.userInfo
     // 因为用户信息初始值是一个对象必须要格式化过,不等于空对象
     return JSON.stringify(state.user.userInfo) !== '{}'
-  }
+  },
+  cssVar: state => variables
 }
 
 export default getters
