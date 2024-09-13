e49be928921bfbcafaa4348c56c8cd3104907c79
Jack <igitinode@gmail.com>
2024-09-13 16:24:24 +0800

    feat(deps): 5.3 基于vue-i18n V9 的国际化最基础用法

--- src/main.js
+++ src/main.js
@@ -13,10 +13,13 @@ import installIcons from '@/icons'
 // 导入路由鉴权
 import './permission'
 
+// 导入 i18n实例
+import i18n from '@/i18n'
+
 const app = createApp(App)
 installElementPlus(app)
 
 // 注册全局内部图标组件
 installIcons(app)
 
-app.use(store).use(router).mount('#app')
+app.use(store).use(router).use(i18n).mount('#app')
