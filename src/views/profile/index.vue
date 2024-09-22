daf6c117f40fbf838c115a77e77dec833281c551
igitinode <igitinode@gmail.com>
2024-09-22 11:40:04 +0800

    feat(components): 5.15 16 方案落地:处理element-plus 主题色变更原理和实现

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -5,6 +5,14 @@
     :total="1000"
   >
   </el-pagination>
+  <el-row>
+    <el-button>Default</el-button>
+    <el-button type="primary">primary</el-button>
+    <el-button type="success">success</el-button>
+    <el-button type="info">info</el-button>
+    <el-button type="warning">warning</el-button>
+    <el-button type="danger">danger</el-button>
+  </el-row>
 </template>
 
 <script setup>
