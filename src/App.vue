295e4bcbb32d0293511461db32cf2090a4c62d3a
igitinode <igitinode@gmail.com>
2024-09-22 20:04:55 +0800

    feat(components): 5.19 方案落地:element-plus 新主题立即生效

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,14 @@
   <router-view />
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
+<script setup>
+import { useStore } from 'vuex'
+import { generateNewStyle, writeNewStyle } from './utils/theme'
+const store = useStore()
+// element-plus 新主题立即生效，手动刷新也生效
+generateNewStyle(store.getters.mainColor).then(nesStyle => {
+  writeNewStyle(nesStyle)
+})
 </script>
 
 <style></style>
