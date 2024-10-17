5434a8ad43b5eaf48eb74cb129ebdba648222770
Jack <igitinode@gmail.com>
2024-10-17 16:34:20 +0800

    feat(components): 7.7 业务落地：文件选择之后的数据解析处理,xlsx FileReader

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -1,10 +1,17 @@
 <template>
-  <upload-excel></upload-excel>
+  <upload-excel :onSuccess="onSuccess"></upload-excel>
 </template>
 
 <script setup>
 import {} from 'vue'
 import UploadExcel from '@/components/UploadExcel'
+
+/**
+ * 数据解析成功之后的回调
+ */
+const onSuccess = excelData => {
+  console.log(excelData)
+}
 </script>
 
 <style lang="scss" scoped></style>
