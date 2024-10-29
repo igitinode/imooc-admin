e2bf7179fa65c23aba6f8fac0eb1700756843030
Jack <igitinode@gmail.com>
2024-10-29 15:24:30 +0800

    feat(components): 9.3 相对时间与时间国际化处理

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,19 +1,36 @@
 // 局部过滤器替代方案
-// // https://v3-migration.vuejs.org/zh/breaking-changes/filters.html
+// https://v3-migration.vuejs.org/zh/breaking-changes/filters.html
+// dayjs 是一个方法
 import dayjs from 'dayjs'
+import rt from 'dayjs/plugin/relativeTime'
+// 导入 简体中文语言包
+import 'dayjs/locale/zh-cn'
+import store from '@/store'
 
 export const dateFilter = (val, format = 'YYYY-MM-DD') => {
   if (!isNaN(val)) {
     val = parseInt(val)
   }
-
   return dayjs(val).format(format)
+}
+
+// 加载相对时间插件
+dayjs.extend(rt)
+// 处理相对时间
+function relativeTime(val) {
+  if (!isNaN(val)) {
+    val = parseInt(val)
+  }
+  return dayjs()
+    .locale(store.getters.language === 'zh' ? 'zh-cn' : 'en')
+    .to(dayjs(val))
 }
 
 // vue3 全局属性
 // https://cn.vuejs.org/api/application#app-config
 export default app => {
   app.config.globalProperties.$filters = {
-    dateFilter
+    dateFilter,
+    relativeTime
   }
 }
