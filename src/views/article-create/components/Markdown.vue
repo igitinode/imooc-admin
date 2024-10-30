116436b3fc231b05544d58a158e56ff9acc7fa48
Jack <igitinode@gmail.com>
2024-10-30 16:57:12 +0800

    feat(components): 10.4 新建文章:markdown实现

--- src/views/article-create/components/Markdown.vue
+++ src/views/article-create/components/Markdown.vue
@@ -1,9 +1,66 @@
 <template>
-  <div class="">markdown</div>
+  <div class="markdown-container">
+    <!-- 渲染区 -->
+    <div id="markdown-box"></div>
+    <!-- 提交按钮 -->
+    <div class="bottom">
+      <el-button type="primary" @click="onSubmitClick">{{
+        $t('msg.article.commit')
+      }}</el-button>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import {} from 'vue'
+import MkEditor from '@toast-ui/editor'
+import '@toast-ui/editor/dist/toastui-editor.css'
+import '@toast-ui/editor/dist/i18n/zh-cn'
+import { onMounted } from 'vue'
+import { useStore } from 'vuex'
+import { watchSwitchLang } from '@/utils/i18n'
+
+// Editor实例
+let mkEditor
+// 处理离开页面切换语言导致 dom 无法被获取
+// el 等到dom渲染完成之后才能调用
+let el
+onMounted(() => {
+  el = document.querySelector('#markdown-box')
+  initEditor()
+})
+
+const store = useStore()
+const initEditor = () => {
+  mkEditor = new MkEditor({
+    el,
+    height: '500px',
+    // 样式
+    previewStyle: 'vertical',
+    // 国际化
+    language: store.getters.language === 'zh' ? 'zh-CN' : 'en'
+  })
+  // 渲染编辑器
+  mkEditor.getMarkdown()
+}
+
+// 语言切换，编辑器的内部功能按钮提示也进行语言变换
+watchSwitchLang(() => {
+  if (!el) return
+  // 先保存用户输入的内容
+  const htmlStr = mkEditor.getHTML()
+  // 清理掉
+  mkEditor.destroy()
+  initEditor()
+  // 把值放回去
+  mkEditor.setHTML(htmlStr)
+})
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.markdown-container {
+  .bottom {
+    margin-top: 20px;
+    text-align: right;
+  }
+}
+</style>
