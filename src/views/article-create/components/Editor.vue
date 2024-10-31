1f582bfca1e693642d6fe192bd1a48a813ac49bc
Jack <igitinode@gmail.com>
2024-10-31 12:12:25 +0800

    feat(components): 10.7 新建文章:富文本实现

--- src/views/article-create/components/Editor.vue
+++ src/views/article-create/components/Editor.vue
@@ -1,9 +1,97 @@
 <template>
-  <div class="">富文本</div>
+  <div class="editor-container">
+    <div id="editor-box"></div>
+    <div class="bottom">
+      <el-button type="primary" @click="onSubmitClick">{{
+        $t('msg.article.commit')
+      }}</el-button>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import {} from 'vue'
+import { watch, onMounted } from 'vue'
+import E from 'wangeditor'
+import { useStore } from 'vuex'
+import { commitArticle, editArticle } from './commit'
+// wangEditor 的 国际化处理
+// https://www.wangeditor.com/doc/pages/12-%E5%A4%9A%E8%AF%AD%E8%A8%80/
+// 官网支持i18next：https://www.i18next.com/
+import i18next from 'i18next'
+const props = defineProps({
+  title: {
+    required: true,
+    type: String
+  },
+  detail: {
+    type: Object
+  }
+})
+
+const emits = defineEmits(['onSuccess'])
+
+const store = useStore()
+
+// Editor实例
+let editor
+// 处理离开页面切换语言导致 dom 无法被获取
+let el
+onMounted(() => {
+  el = document.querySelector('#editor-box')
+  initEditor()
+})
+
+const initEditor = () => {
+  editor = new E(el)
+  editor.config.zIndex = 1
+  // 菜单栏提示
+  editor.config.showMenuTooltips = true
+  editor.config.menuTooltipPosition = 'down'
+  // 国际化相关处理
+  editor.config.lang = store.getters.language === 'zh' ? 'zh-CN' : 'en'
+  editor.i18next = i18next
+  editor.create()
+}
+
+const onSubmitClick = async () => {
+  if (props.detail && props.detail._id) {
+    // 编辑文章
+    await editArticle({
+      id: props.detail._id,
+      title: props.title,
+      content: editor.txt.html()
+    })
+  } else {
+    // 创建文章
+    await commitArticle({
+      title: props.title,
+      content: editor.txt.html()
+    })
+  }
+
+  editor.txt.html('')
+  emits('onSuccess')
+}
+
+// 编辑相关,数据回显
+watch(
+  () => props.detail,
+  val => {
+    if (val && val.content) {
+      editor.txt.html(val.content)
+    }
+  },
+  {
+    immediate: true
+  }
+)
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.editor-container {
+  .bottom {
+    margin-top: 20px;
+    text-align: right;
+  }
+}
+</style>
