54da397818ef295cb73e465177f2da44efde609a
igitinode <igitinode@gmail.com>
2024-11-02 14:50:38 +0800

    feat(components): 14.12 日历模块:日历数据基础渲染 element-plus2 组件

--- src/views/chart/components/calendar/index.vue
+++ src/views/chart/components/calendar/index.vue
@@ -1,12 +1,69 @@
 <template>
-  <div class="container">日历图</div>
+  <el-card
+    class="container"
+    :body-style="{
+      padding: 0
+    }"
+  >
+    <el-calendar class="calendar" v-model="currentDate">
+      <!-- 命名的插槽  -->
+      <template #dateCell="{ data }">
+        <!-- 展示的内容 -->
+        <p :class="[data.isSelected ? 'is-selected' : '']">
+          <!-- 显示的内容 -->
+          {{ data.day.split('-').slice(2).join('') }}
+          <br />
+        </p>
+      </template>
+    </el-calendar>
+  </el-card>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+// 选中的日期
+const currentDate = ref(new Date())
+</script>
 
 <style lang="scss" scoped>
 .container {
   height: 443px;
-  background-color: #bcbfc3;
+  .calendar {
+    ::v-deep .el-calendar__body {
+      padding: 0 12px 12px;
+      font-size: 14px;
+    }
+    ::v-deep .el-calendar-day {
+      height: 66px !important;
+      padding: 0 !important;
+      p {
+        height: 100%;
+        padding: 8px;
+      }
+      // 金额样式
+      .amount {
+        font-size: 12px;
+      }
+      // 正收益
+      .profit {
+        background-color: #f3fff3;
+        span {
+          color: #649840;
+        }
+      }
+      // 负收益
+      .loss {
+        background-color: #ffe7e7;
+        span {
+          color: #b65d59;
+        }
+      }
+
+      .is-selected {
+        background-color: #d6f2ff;
+      }
+    }
+  }
 }
 </style>
