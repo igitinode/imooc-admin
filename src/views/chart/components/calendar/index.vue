b0342151a9c8a97cc42676b507e768d502a3efc4
igitinode <igitinode@gmail.com>
2024-11-02 15:08:40 +0800

    feat(components): 14.13 日历模块:定制数据可视化处理

--- src/views/chart/components/calendar/index.vue
+++ src/views/chart/components/calendar/index.vue
@@ -9,10 +9,19 @@
       <!-- 命名的插槽  -->
       <template #dateCell="{ data }">
         <!-- 展示的内容 -->
-        <p :class="[data.isSelected ? 'is-selected' : '']">
+        <p
+          :class="[
+            data.isSelected ? 'is-selected' : '',
+            calendarItemBgClass(data.day)
+          ]"
+        >
           <!-- 显示的内容 -->
           {{ data.day.split('-').slice(2).join('') }}
           <br />
+          <!-- 当日金额 -->
+          <span class="amount" v-if="getTadayAmount(data.day)">
+            {{ getTadayAmount(data.day) }}
+          </span>
         </p>
       </template>
     </el-calendar>
@@ -21,9 +30,60 @@
 
 <script setup>
 import { ref } from 'vue'
+import { getChartCalendar } from '@/api/chart'
+
+/**
+ * 获取数据
+ */
+const calendarListData = ref([])
+const getCalendarListData = async () => {
+  const { result } = await getChartCalendar()
+  calendarListData.value = result
+}
+getCalendarListData()
 
 // 选中的日期
 const currentDate = ref(new Date())
+
+// 收益缓存数据。
+// key 为日期
+// val 为对应的 金额
+// 数据较多，减少运算
+const dateAmountList = ref({})
+
+/**
+ * 返回指定日期收益数据，在日历中进行展示
+ */
+const getTadayAmount = date => {
+  // 读取缓存数据
+  if (dateAmountList.value[date]) {
+    return dateAmountList.value[date]
+  }
+  // 根据日期，获取当日数据
+  const tadayData = calendarListData.value.find(item => item.date === date)
+  // 判断当日数据是否存在
+  if (!tadayData) {
+    return 0
+  }
+  // 获取当日收益
+  const tadayAmount = tadayData.amount
+
+  // 对新的数据进行缓存
+  dateAmountList.value[date] = tadayAmount
+  return tadayAmount
+}
+
+/**
+ * 返回日历的背景
+ * */
+const calendarItemBgClass = day => {
+  if (getTadayAmount(day) > 0) {
+    return 'profit'
+  } else if (getTadayAmount(day) < 0) {
+    return 'loss'
+  }
+  return ''
+}
 </script>
 
 <style lang="scss" scoped>
